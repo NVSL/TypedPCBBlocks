e4ff77f74d2b892ff7abd593ef3c566a876a1aec
J. Garza <jgarzagu@eng.ucsd.edu>
2021-10-28 23:41:46 -0700

    Adding Python Tuples format anotation in sch

--- src/server/schematicToTyped/schematicToTyped.py
+++ src/server/schematicToTyped/schematicToTyped.py
@@ -9,8 +9,11 @@ Usage:
 Options:
   -i         Input Schematic
 """
+from __future__ import print_function
+import sys
 import json
 import re
+from ast import literal_eval
 from types import resolve_bases
 from Swoop import Swoop
 from docopt import docopt
@@ -22,6 +25,8 @@ def main(arguments):
   if arguments['-i'] and arguments['SCHEMATIC_FILE'] is True:
     print(arguments['SCHEMATIC_FILE'])
 
+  def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
   def getProtocolNumber(protocolAndNumber):
     for charNum in reversed(range(len(protocolAndNumber))):
@@ -32,17 +37,31 @@ def main(arguments):
   boardFile = Swoop.EagleFile.from_file(arguments['SCHEMATIC_FILE']);
   # print(boardFile.__dict__.keys())
 
+  # Typed dictionary
   typedSchematic = [{}]
+
+
   for sheet in boardFile.sheets:
     for net in sheet.nets:
+
+      protocolAndNumber = ''
+      protocol = ''
+      protocolNumber = ''
+      protocolSignal = ''
+      protocolVoltage = ''
+      protocolVoltageType = ''
+      foundTypedNet = False
+      # Minimal typed information
       if('#' in net):
+        foundTypedNet = True
+
         # Get schematic typed data
         protocolAndNumber = net.partition('_')[0].partition('.')[0].partition('#')[2]
         protocolNumber = getProtocolNumber(protocolAndNumber)
         if not protocolNumber:
           protocolNumber = "0"
         protocol = protocolAndNumber.partition(protocolNumber)[0]
-        protocolSubnet = net.partition('_')[0].partition('.')[2].partition('_')[0]
+        protocolSignal = net.partition('_')[0].partition('.')[2].partition('_')[0]
 
         # Check if voltage is one number, a range or a list
         resVoltage = net.partition('_')[2]
@@ -62,15 +81,49 @@ def main(arguments):
         # print(protocol)
         # print(protocolNumber)
         # print(protocolAndNumber)
-        # print(protocolSubnet)
+        # print(protocolSignal)
         # print(protocolVoltage)
+
+      # Extended typed information
+      if('[' and ']' in net):
+        foundTypedNet = True
         
-        # Create dictionary
+        netDictionary = dict(literal_eval(net))
+
+        if('TYPE' in netDictionary):
+          protocol = netDictionary['TYPE']
+        else:
+          eprint('Missing protocol type in', net)
+          quit()
+
+        if ('NUMBER' in netDictionary):
+          protocolAndNumber = netDictionary['TYPE']+str(netDictionary['NUMBER'])
+          protocolNumber = str(netDictionary['NUMBER'])
+        else:
+          protocolAndNumber = netDictionary['TYPE']+'0'
+          protocolNumber = '0'
+
+        if ('SIGNAL' in netDictionary):
+          protocolSignal = netDictionary['SIGNAL']
+        else:
+          protocolSignal = ''
+
+        if ('VOLTAGE' in netDictionary):
+          protocolVoltage = str(netDictionary['VOLTAGE'])
+        else:
+          protocolVoltage = ''
+
+        protocolVoltageType = '' # TODO
+
+      if (foundTypedNet):
+        # Create dictionary if doesn't exists for protocol
         if(protocolAndNumber not in typedSchematic[0]):
           typedSchematic[0][protocolAndNumber] = []
 
         # Add Typed information
-        typedSchematic[0][protocolAndNumber].append({'protocol':protocol, 'number': protocolNumber, 'net': protocolSubnet, 'voltageType': protocolVoltageType, 'voltage': protocolVoltage})
+        typedSchematic[0][protocolAndNumber].append({'protocol': { 'type': protocol, 'number': protocolNumber, 'signal': protocolSignal }, 
+        'voltage': {'type': protocolVoltageType, 'voltage': protocolVoltage}})
+        
 
   # Write Typed Datas
   with open('typedFile.json', 'w') as typedFile:
