fe0797abaabab7277b8854d4a85c132b799db8c2
J. Garza <jgarzagu@eng.ucsd.edu>
2021-11-09 23:32:57 -0800

    Adding more merger code

--- src/server/schematicToTyped/typedSchematicsMerger.py
+++ src/server/schematicToTyped/typedSchematicsMerger.py
@@ -14,12 +14,16 @@ Options:
 
 import json
 import os
+import sys
 import shutil
 from docopt import docopt
 from Swoop import Swoop
 
 _debugMode = False
 _arguments = ""
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 def debug_print(*args, sep=' ', end='\n', file=None):
     if _debugMode is True:
@@ -34,25 +38,113 @@ def main(arguments):
         with open(arguments['JSON_FILE'], 'r') as f:
             deviceConnections = json.loads(f.read())
 
-    print(deviceConnections)
 
-    schematic = Swoop.EagleFile.from_file(pyPath + 'typedSchematics/temperature_sensor.sch')
-    net_names = Swoop.From(schematic).get_sheets().get_nets().get_name()
-    print(net_names)
+    # Get unique schematics and give them a unique number
+    unique_schematics = {}
+    unique_counter = 0
+    for connections in deviceConnections:
+        print(connections['connect'])
+        for connection in connections['connect']:
+            schematic = connection['schematic']
+            if (schematic not in unique_schematics):
+                unique_schematics[schematic] = "__u"+str(unique_counter)
+                unique_counter+=1;
 
-    # Rename nets
-    # for net_name, connection in interface_info.items(): # each entry is a potential net
-    #     if net_name in net_names:
-    #         debug_print('Connecting net:', net_name, 'to', connection)
-    #         # get the net
-    #         net = (Swoop.
-    #             From(unique_schematics[module_name]).
-    #             get_sheets().
-    #             get_nets().
-    #             with_name(net_name)
-    #         )[0]
-    #         net.set_name(connection) # rename the net
-    #         renamed_nets[module_name].add(connection)
+    print("UNIQUE SCHEMARICS: \n", unique_schematics)
+
+
+    # Clone schematics and save them locally
+    schematic_data= {}
+    for schematic in unique_schematics:
+        data = Swoop.EagleFile.from_file(pyPath + "typedSchematics/" + schematic + '.sch')
+        schematic_data[schematic] = data
+
+    # Connect Nets
+    for connections in deviceConnections:
+        # Create renamed net name
+        renamedNet = ''
+        for connection in connections['connect']:
+            if renamedNet != '':
+                renamedNet += '+' + connection['net']
+            else:
+                renamedNet += connection['net']
+        # Rename net
+        for connection in connections['connect']:
+            net = (Swoop.
+                From(schematic_data[connection['schematic']]).
+                get_sheets().
+                get_nets().
+                with_name(connection['net']))[0]
+            net.set_name(renamedNet)
+
+    # Make rest of nets unique
+    for schematic in unique_schematics:
+        for net in Swoop.From(schematic_data[schematic]).get_sheets().get_nets():
+            if ('+' not in net.name):
+                net.set_name(net.name+unique_schematics[schematic])
+            print(schematic, net.name)
+
+    # Make parts library unique
+    for schematic in unique_schematics:
+        for part in Swoop.From(schematic_data[schematic]).get_parts():
+            print('Looking at part:', part)
+            old_name = part.name
+            schematic_data[schematic].remove_part(part)
+            part.name = old_name + '_' + unique_schematics[schematic]
+            schematic_data[schematic].add_part(part)
+
+            for instance in Swoop.From(schematic_data[schematic]).get_sheets().get_instances():
+                    # print('instance:', instance.part)
+                    if instance.part == old_name:
+                        instance.part = part.name
+
+            for pinref in Swoop.From(schematic_data[schematic]).get_sheets().get_nets().get_segments().get_pinrefs():
+                # print('pinref:', pinref)
+                if pinref.part == old_name:
+                    pinref.part = part.name
+        # print(Swoop.From(schematic_data[schematic]).get_libraries().get_devicesets())
+
+    # debug_print('Renamed all parts, verifying')
+
+    
+    emptySchematic = Swoop.EagleFile.from_file(pyPath + 'typedSchematics/_emptyTemplate.sch')
+    print(emptySchematic)
+
+    for schematic in unique_schematics:
+        sheets = schematic_data[schematic].get_sheets()
+        print(sheets)
+        for sheet in sheets:
+            emptySchematic.add_sheet(sheet)
+
+        for part in schematic_data[schematic].get_parts():
+            library_name = part.get_library()
+            library = schematic_data[schematic].get_library(library_name)
+            print("library", library.__dict__)
+            # devicesets = library.get_devicesets()
+            # print("DeviceSets:", devicesets)
+            # print(library)
+            emptySchematic.add_part(part)
+            emptySchematic.add_library(library)
+            print("////", (Swoop.From(emptySchematic).get_libraries()).count())
+            # 
+            # for deviceset in devicesets:
+            #     library.add_deviceset(deviceset)
+
+        # for deviceset in Swoop.From(schematic_data[schematic]).get_libraries().get_devicesets():
+        #     emptySchematic.add_deviceset(deviceset)
+        
+    # print(emptySchematic.get_sheets())
+
+    # Todo: R4__U1 not found
+
+    print("---")
+    print("---")
+    print(Swoop.From(emptySchematic).get_libraries())
+
+    # for deviceset in Swoop.From(emptySchematic).get_libraries().get_devicesets():
+    #     print(deviceset)
+
+    # emptySchematic.write(pyPath + 'Combined.sch')
 
 
 if __name__ == '__main__':
