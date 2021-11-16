9dd17f10c840169a732bd16a7b41bb9ff64e45b0
J. Garza <jgarzagu@eng.ucsd.edu>
2021-11-15 22:54:44 -0800

    Starting board block design transfer

--- src/server/schematicToTyped/typedSchematicsMerger.py
+++ src/server/schematicToTyped/typedSchematicsMerger.py
@@ -45,6 +45,135 @@ def getPrefix(unique_schematics, schematic, instance):
             if (data["schematic"] == schematic and data["instance"] == instance):
                 return key
 
+
+##### BRD START
+def rebuildBoardConnections(sch, brd):
+    """
+    Update the signals in :code:`brd` to match the nets in :code:`sch`.  This will set up the connections, but won't draw the air wires.  You can use Eagle's :code:`ripup` command to rebuild those.
+
+    :param sch: :class:`SchematicFile` object and source of the connection information.
+    :param brd: :class:`BoardFile` destination for then connection information.
+    :rtype: :code:`None`
+    
+    """
+    #sheets/*/net.name:
+    for name in Swoop.From(sch).get_sheets().get_nets().get_name():
+        sig =  brd.get_signal(name)
+        if sig is None:
+            brd.add_signal(Swoop.Signal().
+                           set_name(name).
+                           set_airwireshidden(False).
+                           set_class("0")) # We need to do something smarter here.
+        else:
+            sig.clear_contactrefs()
+
+        for pinref in (Swoop.From(sch).
+                       get_sheets().
+                       get_nets().
+                       with_name(name).
+                       get_segments().
+                       get_pinrefs()):
+
+            try:
+                if sch.get_part(pinref.part).find_device().get_package() is None:
+                    continue
+            except:
+                continue
+
+            pads = (Swoop.From(sch).
+                   get_parts().
+                   with_name(pinref.get_part()).
+                   find_device().
+                   get_connects().
+                   with_gate(pinref.gate).
+                   with_pin(pinref.pin).
+                   get_pads())
+
+            assert pads is not None;
+            if pads is None:
+                eprint("Can't find pads for '{}:{}.{}' on net '{}'".format(pinref.get_part(), pinref.gate, pinref.pin, name))
+
+            for pad in pads:
+                brd.get_signal(name).add_contactref(Swoop.Contactref().
+                                                    set_element(pinref.get_part()).
+                                                    set_pad(pad))
+
+def propagatePartToBoard(part, brd):
+
+    """
+    Copy :code:`part` to ``brd`` by creating a new :class:`Element` and populating it accordingly.
+    If the part already exists, it will be replaced.  Attributes are not displayed by default, but the display layer is set to "Document".
+    
+    If the library for the part is missing in the board, it will be create.  If the package is missing, it will be copied.  If it exists and the package for the part and the package in the board are not the same, raise an exception.
+
+    .. Note::
+       This function doesn't update the board's signals.  You can run :meth:`rebuildBoardConnections` to do that.
+
+    :param part: :class:`Part` object that to propagate.
+    :param brd: Destination :class`BoardFile`.
+    :rtype: :code:`None`
+
+    """
+    try:
+        if part.find_device().get_package() is None:
+            return
+    except:
+        return
+    
+    if part.find_package() is None:
+        raise Swoop.SwoopError("Can't find package for '{}' ({}.{}.{}.{}).".format(part.get_name(), part.get_library(), part.get_deviceset(), part.get_device(), part.get_technology()))
+
+    dst_lib = brd.get_library(part.get_library())
+
+    if dst_lib is None:
+        dst_lib = Swoop.Library().set_name(part.get_library())
+        brd.add_library(dst_lib)
+
+    #src_lib = part.find_library()
+    #assert src_lib is not None, "Missing library '{}' for part '{}'".format(part.get_library(), part.get_name())
+    
+    dst_package = dst_lib.get_package(part.find_package().get_name())
+    if dst_package is None:
+        dst_package = part.find_package().clone()
+        dst_lib.add_package(dst_package)
+    else:
+        assert dst_package.is_equal(part.find_package()), "Package from schematic is not the same as package in board"
+
+    # Reverse-engineered logic about setting values in board files.
+    if part.find_deviceset().get_uservalue():
+        fallback_value = ""
+    else:
+        fallback_value = part.get_deviceset()+part.get_device()
+    
+    n =(Swoop.Element().
+        set_name(part.get_name()).
+        set_library(part.get_library()).
+        set_package(part.
+                    find_package().
+                    get_name()).
+        set_value(part.get_value() if part.get_value() is not None else fallback_value).
+        set_x(0).
+        set_y(0))
+
+    brd.add_element(n)
+
+
+def build_board_from_schematic(sch, template_brd):
+    """
+    Create a minimal board from a schematic file.  :code:`template_brd` is modified and returned.
+    
+    :param sch: the input schematic
+    :param brd: a template :class:`BoardFile`
+    :returns: A :class:`BoardFile` object that is consistent with the schematic.
+    """
+    for part in sch.get_parts():
+        propagatePartToBoard(part, template_brd)
+
+    rebuildBoardConnections(sch, template_brd)
+    return template_brd
+
+#####  BRD END
+
 def main(arguments):
     pyPath = os.path.dirname(os.path.realpath(__file__)) + "/"
 
@@ -173,13 +302,37 @@ def main(arguments):
             emptySchematic.add_library(library)
 
     # Save new schematic
-    emptySchematic.write(pyPath + _mergedSchematicName)
+    # emptySchematic.write(pyPath + _mergedSchematicName)
     debug_print(">> Schematic write succesfully!")
 
 
     ################ Board Merger ####################
     # TODO: Add this to another file
 
+
+
+    # Make board from schematic
+    brdTemplate = Swoop.EagleFile.from_file(pyPath + 'typedSchematics/_emptyTemplate.brd')
+    board = build_board_from_schematic(emptySchematic, brdTemplate)
+
+    # Transfer modules designs
+    for prefix, dicValue in unique_schematics.items():
+        schematic = dicValue["schematic"]
+        debug_print(dicValue['schematic'], prefix,":")
+        moduleBrd = Swoop.EagleFile.from_file(pyPath + 'typedSchematics/'+ schematic +'.brd')
+        for moduleEleName,moduleEle in moduleBrd.elements.items():
+            print(moduleEleName, moduleEle.name, moduleEle.x, moduleEle.y)
+            for boardEleName,boardEle in board.elements.items():
+                if (boardEleName == moduleEleName+prefix ):
+                    print("Found!", boardEleName)
+                    # Copy element postions
+                    boardEle.x = moduleEle.x
+                    boardEle.y = moduleEle.y
+                    boardEle.rot = moduleEle.rot
+            # TODO: Add signals, attribute names and define a X position (maybe from json)
+    
+    # Write final board file
+    board.write(pyPath + 'COMBINED.brd')
 
     
 
