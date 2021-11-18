709132e21c7438333702b19d986842c6766c8c0f
J. Garza <jgarzagu@eng.ucsd.edu>
2021-11-17 20:50:51 -0800

    Adding dynamic imports for protocol defs

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,83 +1,80 @@
-type net = boolean;
+import { PROPS } from './data/typedDefinitions/PROTOCOL'; // TODO: Add as a package
 
-interface SPI {
-    MISO: boolean;
-    MOSI: boolean;
-    SCK: boolean;
+function loadProtocol(protocol: any, typedJson: any) {
+  const loadedProtocol = Object.assign(protocol, typedJson);
+  return loadedProtocol;
 }
 
-class POWER {
-    netVoltage: number | null = null
-    sourceVoltage: number | null = null
-    constructor(props: PROPS) {
-        this.sourceVoltage = props.sourceVoltage;
-    }
+async function makeConnections(
+  props: PROPS,
+  protocolStr: string,
+  typedJsonOne: any,
+  typedJsonTwo: any,
+) {
+  const path = './data/typedDefinitions/';
+  const protocol = await import(path + protocolStr);
+  console.log(protocol);
+  const netOne = loadProtocol(new protocol[protocolStr](props), typedJsonOne);
+  const netTwo = loadProtocol(new protocol[protocolStr](props), typedJsonTwo);
+  console.log(netOne);
+  console.log(netTwo);
+
+  // Connect:
+  netOne.connect(netTwo);
 }
 
-interface PROPS {
-    sourceVoltage: number;
-} 
+// Main program
+(async () => {
+  // Typed NET ONE
+  const typedJsonOne = {
+    MISO: true,
+    MOSI: true,
+    SCK: true,
+    netVoltage: 3.3,
+  };
 
+  // Typed NET TWO
+  const typedJsonTwo = {
+    MISO: true,
+    MOSI: true,
+    SCK: true,
+    netVoltage: 3.3,
+  };
 
-class SPI extends POWER { 
-    MISO: net = false;
-    MOSI: net = false;
-    SCK: net = false;
-    constructor(props: PROPS) {
-        super(props)
-        console.log(props)
-    }
-    public connect(net2: SPI): boolean {
-        // Add constrains
-        return false;
-    }
-}
+  // Global propagation properties
+  const props: PROPS = {
+    sourceVoltage: 3.3,
+  };
 
+  await makeConnections(props, 'SPI', typedJsonOne, typedJsonTwo);
+})();
 
-// Net connect checker
-const jsonTypedTest = {
-    MISO : true,
-    MOSI : true,
-    netVoltage: 5
-}
-
-const porps: PROPS = {
-    sourceVoltage: 3.3
-}
-
-const iface = Object.assign( new SPI(porps), jsonTypedTest )
-console.log(iface)
-// const spi = new SPI(jsonTypedTest)
-
-
-
-
-const jsonTypedSchematic = {"SPI-0": [
-    {
-        "ALTNAME": "0",
-        "NET": "#SPI0.MOSI||#GPIO11",
-        "SIGNAL": "MOSI",
-        "TYPE": "SPI",
-        "VOLTAGE": "",
-        "VOLTAGE_LIST": "",
-        "VOLTAGE_RANGE": ""
-    },
-    {
-        "ALTNAME": "0",
-        "NET": "#SPI0.MISO||#GPIO12",
-        "SIGNAL": "MISO",
-        "TYPE": "SPI",
-        "VOLTAGE": "",
-        "VOLTAGE_LIST": "",
-        "VOLTAGE_RANGE": ""
-    },
-    {
-        "ALTNAME": "0",
-        "NET": "#SPI0.SCK||#GPIO13",
-        "SIGNAL": "SCK",
-        "TYPE": "SPI",
-        "VOLTAGE": "",
-        "VOLTAGE_LIST": "",
-        "VOLTAGE_RANGE": ""
-    }
-]}
+// const jsonTypedSchematic = {"SPI-0": [
+//     {
+//         "ALTNAME": "0",
+//         "NET": "#SPI0.MOSI||#GPIO11",
+//         "SIGNAL": "MOSI",
+//         "TYPE": "SPI",
+//         "VOLTAGE": "",
+//         "VOLTAGE_LIST": "",
+//         "VOLTAGE_RANGE": ""
+//     },
+//     {
+//         "ALTNAME": "0",
+//         "NET": "#SPI0.MISO||#GPIO12",
+//         "SIGNAL": "MISO",
+//         "TYPE": "SPI",
+//         "VOLTAGE": "",
+//         "VOLTAGE_LIST": "",
+//         "VOLTAGE_RANGE": ""
+//     },
+//     {
+//         "ALTNAME": "0",
+//         "NET": "#SPI0.SCK||#GPIO13",
+//         "SIGNAL": "SCK",
+//         "TYPE": "SPI",
+//         "VOLTAGE": "",
+//         "VOLTAGE_LIST": "",
+//         "VOLTAGE_RANGE": ""
+//     }
+// ]}
