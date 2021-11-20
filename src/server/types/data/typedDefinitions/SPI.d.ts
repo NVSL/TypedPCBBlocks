488a3d362aaa84bed11ecff814cb1dd0ff430f2e
J. Garza <jgarzagu@eng.ucsd.edu>
2021-11-19 18:21:39 -0800

    Refactor folders, removing src

--- src/server/types/data/typedDefinitions/SPI.d.ts
+++
@@ -1,9 +0,0 @@
-import { PROTOCOL, POWER, PROPS, net } from './PROTOCOL';
-declare class SPI extends POWER implements PROTOCOL<SPI> {
-    MISO: net;
-    MOSI: net;
-    SCK: net;
-    constructor(props: PROPS);
-    connect(net: SPI): boolean;
-}
-export { SPI };
