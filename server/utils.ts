4f887fe1d9c61b41eba8821ef1e92572ba88a6cd
J. Garza <jgarzagu@eng.ucsd.edu>
2022-01-14 22:11:17 -0800

    Adding Multi-Key Hash Map

--- server/utils.ts
+++ server/utils.ts
@@ -20,4 +20,45 @@ class Queue {
   }
 }
 
-export { Queue };
+class MultiMap<K, V> {
+  map: Map<string, V>;
+  constructor() {
+    this.map = new Map();
+  }
+  public set(key: K, val: V): Map<string, V> {
+    const jsonKey: string = JSON.stringify(key);
+    return this.map.set(jsonKey, val);
+  }
+  public get(key: K): V | null {
+    const jsonKey: string = JSON.stringify(key);
+    const val = this.map.get(jsonKey);
+    return val ? val : null;
+  }
+  public has(key: K): boolean {
+    const jsonKey: string = JSON.stringify(key);
+    return this.map.has(jsonKey);
+  }
+  public entries(): IterableIterator<[string, V]> {
+    return this.map.entries();
+  }
+  public values(): IterableIterator<V> {
+    return this.map.values();
+  }
+  public keys(): IterableIterator<string> {
+    return this.map.keys();
+  }
+  public forEach(
+    callbackfn: (value: V, key: string, map: Map<string, V>) => void,
+    thisArg?: any,
+  ): void {
+    this.map.forEach(callbackfn, thisArg);
+  }
+  public clear(): void {
+    this.map.clear();
+  }
+  public get size(): number {
+    return this.map.size;
+  }
+}
+
+export { Queue, MultiMap };
