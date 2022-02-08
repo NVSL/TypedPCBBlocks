3100dd0b3f6a4901749b7c93b0c17ec84925bfe8
J. Garza <jgarzagu@eng.ucsd.edu>
2022-02-07 23:19:17 -0800

    Adding SchemaFlow Example

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,68 +1,92 @@
-import './style.css';
-import { Tscheda, TschedaDebug } from 'tscheda';
+// import './style.css';
+// Tsch EDA
+// import { Tscheda, TschedaDebug } from 'tscheda';
+// Schema Flow
+import SchemaFlow from './SchemaFlow/SchemaFlow';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+var id = <HTMLElement>document.getElementById('app');
+const editor = new SchemaFlow(id);
+editor.start();
 
-app.innerHTML = `
-  <h1>Hello Vite?</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+var computeModule = `
+      <div>
+        <div class="title-box"><i class="fas fa-code"></i> Compute Module</div>
+        <div class="box">
+          <textarea df-template></textarea>
+        </div>
+      </div>
+      `;
+editor.addNode(
+  'computeModule',
+  { 1: 'GPIO' },
+  {
+    1: { name: 'I2C', max: 4 },
+    2: { name: 'GPIO', max: 2 },
+    3: { name: 'SPI', max: 1 },
+    4: { name: 'UART', max: 1 },
+  }, // 1:[type, max_connections]
+  10,
+  10,
+  'computeModule',
+  { template: 'Schematic here!' },
+  computeModule,
+);
 
-async function readURLFile(path: string) {
-  let text = '';
-  const respionse = await fetch(new Request(path));
-  if (!respionse.ok) throw new Error(respionse.statusText);
-  text = await respionse.text();
-  return text;
-}
+// async function readURLFile(path: string) {
+//   let text = '';
+//   const respionse = await fetch(new Request(path));
+//   if (!respionse.ok) throw new Error(respionse.statusText);
+//   text = await respionse.text();
+//   return text;
+// }
 
-async function eagelFile(
-  filename: string,
-): Promise<{ data: string; filename: string }> {
-  const tschPath = '../data/typedSchematics/';
-  const data = await readURLFile(tschPath + filename);
-  return { data: data, filename: filename };
-}
+// async function eagelFile(
+//   filename: string,
+// ): Promise<{ data: string; filename: string }> {
+//   const tschPath = '../data/typedSchematics/';
+//   const data = await readURLFile(tschPath + filename);
+//   return { data: data, filename: filename };
+// }
 
-async function led(): Promise<void> {
-  console.log('\n--- LED DESIGN');
-  try {
-    const tscheda = new Tscheda('http://localhost:3000/data/typedConstraints/');
-    const atmega328 = await tscheda.use(await eagelFile('atmega328.sch'));
-    const led = await tscheda.use(await eagelFile('led_smd.sch'));
-    const power5V12V = await tscheda.use(await eagelFile('power5V12V.sch'));
-    const power5V = await tscheda.use(await eagelFile('power5V.sch'));
+// async function led(): Promise<void> {
+//   console.log('\n--- LED DESIGN');
+//   try {
+//     const tscheda = new Tscheda('http://localhost:3000/data/typedConstraints/');
+//     const atmega328 = await tscheda.use(await eagelFile('atmega328.sch'));
+//     const led = await tscheda.use(await eagelFile('led_smd.sch'));
+//     const power5V12V = await tscheda.use(await eagelFile('power5V12V.sch'));
+//     const power5V = await tscheda.use(await eagelFile('power5V.sch'));
 
-    const Mat5V12V = tscheda.newMat(power5V12V);
-    const Mat5V = tscheda.newMat(power5V);
+//     const Mat5V12V = tscheda.newMat(power5V12V);
+//     const Mat5V = tscheda.newMat(power5V);
 
-    tscheda.addMat('root', Mat5V12V);
-    tscheda.addMat(Mat5V12V, Mat5V);
+//     tscheda.addMat('root', Mat5V12V);
+//     tscheda.addMat(Mat5V12V, Mat5V);
 
-    tscheda.addTsch(Mat5V, atmega328);
-    tscheda.addTsch(Mat5V, led);
+//     tscheda.addTsch(Mat5V, atmega328);
+//     tscheda.addTsch(Mat5V, led);
 
-    await tscheda.connect({ uuid: atmega328, protocol: 'GPIO-9' }, [
-      { uuid: led, protocol: 'GPIO-0' },
-    ]);
+//     await tscheda.connect({ uuid: atmega328, protocol: 'GPIO-9' }, [
+//       { uuid: led, protocol: 'GPIO-0' },
+//     ]);
 
-    tscheda.printConnectionMap();
+//     tscheda.printConnectionMap();
 
-    tscheda.drc();
+//     tscheda.drc();
 
-    const jsonData = tscheda.generateJson();
-    console.log('JSON OUTPUT', jsonData);
-  } catch (e) {
-    throw e;
-  }
+//     const jsonData = tscheda.generateJson();
+//     console.log('JSON OUTPUT', jsonData);
+//   } catch (e) {
+//     throw e;
+//   }
 
-  return;
-}
+//   return;
+// }
 
-(async () => {
-  TschedaDebug.enable(true, 1);
-  await led();
-  /* TODO: 
-  - Start UI
-  */
-})();
+// (async () => {
+//   TschedaDebug.enable(true, 1);
+//   await led();
+//   /* TODO:
+//   - Start UI
+//   */
+// })();
