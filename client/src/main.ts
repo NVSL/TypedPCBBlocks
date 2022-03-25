a02ffa231cbb97590303dedbf8fa49bd54adeb89
J. Garza <jgarzagu@eng.ucsd.edu>
2022-03-24 23:27:04 -0700

    Changing example to us use TschedaFlow

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,86 +1,97 @@
-import { Flow } from './Flow/Flow';
-// ### UI Interface
+import { TschedaFlow } from './TschedaFlow/TschedaFlow';
 
 const container = <HTMLElement>document.querySelector('#tschs');
-const flow = new Flow(container);
 
-// Button AddMat
-document.querySelector('#addMatTsch')!.addEventListener('click', () => {
-  // flow.addMatTsch();
-});
-
-// Button AddTsch
-document.querySelector('#addBlockTsch')!.addEventListener('click', () => {
-  // flow.addBlockTsch();
-});
-
-// Add Node
-var computeModule = `
-      <div>
-        <div class="title-box"><i class="fas fa-code"></i> Compute Module</div>
-      </div>
-      `;
-flow.addNode(
-  'BlockTsch',
-  { 1: { name: 'GPIO', max: 2 } },
-  {
-    1: { name: 'GPIO', max: 2 },
-    2: { name: 'I2C', max: 2 },
-    3: { name: 'SPI', max: 2 },
-    4: { name: 'UART', max: 2 },
-  }, // 1:[type, max_connections]
-  100,
-  100,
-  'computeModule',
-  computeModule,
+const tschedaFlow = new TschedaFlow(
+  container,
+  'http://localhost:3000/data/typedConstraints/',
 );
 
-var pheripherial = `
-      <div>
-        <div class="title-box"><i class="fas fa-code"></i> Pheripherial</div>
-      </div>
-      `;
-flow.addNode(
-  'BlockTsch',
-  {
-    1: { name: 'GPIO', max: 2 },
-    2: { name: 'I2C', max: 2 },
-    3: { name: 'SPI', max: 2 },
-    4: { name: 'UART', max: 2 },
-  }, // 1:[type, max_connections]
-  {},
-  100,
-  500,
-  'pheripherial',
-  pheripherial,
-);
+tschedaFlow.addTypedSchematic('atmega328.sch');
 
-const matModule5V = `
-      <div>
-        <div class="title-box"><i class="fas fa-code"></i>MAT 5V</div>
-      </div>
-      `;
-flow.addNode(
-  'MatTsch',
-  { 1: { name: 'I2C', max: 2 } },
-  { 1: { name: 'GPIO', max: 2 } }, // 1:[type, max_connections]
-  500,
-  100,
-  '',
-  matModule5V,
-);
+// import { Flow } from './Flow/Flow';
+// // ### UI Interface
 
-const matModule3V3 = `
-      <div>
-        <div class="title-box"><i class="fas fa-code"></i>MAT 3.3V</div>
-      </div>
-      `;
-flow.addNode(
-  'MatTsch',
-  {},
-  {}, // 1:[type, max_connections]
-  300,
-  100,
-  '',
-  matModule3V3,
-);
+// const container = <HTMLElement>document.querySelector('#tschs');
+// const flow = new Flow(container);
+
+// // Button AddMat
+// document.querySelector('#addMatTsch')!.addEventListener('click', () => {
+//   // flow.addMatTsch();
+// });
+
+// // Button AddTsch
+// document.querySelector('#addBlockTsch')!.addEventListener('click', () => {
+//   // flow.addBlockTsch();
+// });
+
+// // Add Node
+// var computeModule = `
+//       <div>
+//         <div class="title-box"><i class="fas fa-code"></i> Compute Module</div>
+//       </div>
+//       `;
+// flow.addNode(
+//   'BlockTsch',
+//   { 1: { name: 'GPIO', max: 2 } },
+//   {
+//     1: { name: 'GPIO', max: 2 },
+//     2: { name: 'I2C', max: 2 },
+//     3: { name: 'SPI', max: 2 },
+//     4: { name: 'UART', max: 2 },
+//   }, // 1:[type, max_connections]
+//   100,
+//   100,
+//   'computeModule',
+//   computeModule,
+// );
+
+// var pheripherial = `
+//       <div>
+//         <div class="title-box"><i class="fas fa-code"></i> Pheripherial</div>
+//       </div>
+//       `;
+// flow.addNode(
+//   'BlockTsch',
+//   {
+//     1: { name: 'GPIO', max: 2 },
+//     2: { name: 'I2C', max: 2 },
+//     3: { name: 'SPI', max: 2 },
+//     4: { name: 'UART', max: 2 },
+//   }, // 1:[type, max_connections]
+//   {},
+//   100,
+//   500,
+//   'pheripherial',
+//   pheripherial,
+// );
+
+// const matModule5V = `
+//       <div>
+//         <div class="title-box"><i class="fas fa-code"></i>MAT 5V</div>
+//       </div>
+//       `;
+// flow.addNode(
+//   'MatTsch',
+//   { 1: { name: 'I2C', max: 2 } },
+//   { 1: { name: 'GPIO', max: 2 } }, // 1:[type, max_connections]
+//   500,
+//   100,
+//   '',
+//   matModule5V,
+// );
+
+// const matModule3V3 = `
+//       <div>
+//         <div class="title-box"><i class="fas fa-code"></i>MAT 3.3V</div>
+//       </div>
+//       `;
+// flow.addNode(
+//   'MatTsch',
+//   {},
+//   {}, // 1:[type, max_connections]
+//   300,
+//   100,
+//   '',
+//   matModule3V3,
+// );
