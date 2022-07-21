992c1f5be9c7debe6d2b98b39c3a1e96e56ce89d
J. Garza <jgarzagu@eng.ucsd.edu>
2022-07-20 19:42:19 -0700

    Adding schematic blocks side panel

--- client/src/main.ts
+++ client/src/main.ts
@@ -43,127 +43,176 @@ tschedaFlow.addTypedSchematic('led_smd.sch', 20, 380);
 tschedaFlow.addTypedSchematic('temperature_sensor.sch', 20, 600);
 tschedaFlow.addTypedSchematic('flash.sch', 20, 1000);
 tschedaFlow.addTypedSchematic('power5V12V.sch', 100, 400);
-tschedaFlow.addTypedSchematic('power5V.sch', 600, 100);
-tschedaFlow.addTypedSchematic('power3V3.sch', 800, 100);
+tschedaFlow.addTypedSchematic('power5V.sch', 400, 100);
+tschedaFlow.addTypedSchematic('power3V3.sch', 400, 100);
 
-// Generate Schematic
-document
-  .querySelector('#buttonGenerateSchematic')!
-  .addEventListener('click', async () => {
-    const data = tschedaFlow.generateJSONSchematic();
-    console.log(JSON.stringify(data, null, 4));
-    // const data = [
-    //   {
-    //     type: 'VOUT',
-    //     wire: null,
-    //     connect: [
-    //       {
-    //         schematic: 'power5V12V.sch',
-    //         instance: 1,
-    //         net: '@VOUT_5V-12V',
-    //       },
-    //       {
-    //         schematic: 'power5V.sch',
-    //         instance: 1,
-    //         net: '@VIN_5V-15V',
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     type: 'VOUT',
-    //     wire: null,
-    //     connect: [
-    //       {
-    //         schematic: 'power5V.sch',
-    //         instance: 1,
-    //         net: '@VOUT_5V',
-    //       },
-    //       {
-    //         schematic: 'temperature_sensor.sch',
-    //         instance: 1,
-    //         net: '@VIN_2.7V-5.5V',
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     type: 'I2C',
-    //     wire: 'SDA',
-    //     connect: [
-    //       {
-    //         schematic: 'atmega328.sch',
-    //         instance: 1,
-    //         net: '#I2C-0.SDA!||#ADC-4!',
-    //       },
-    //       {
-    //         schematic: 'temperature_sensor.sch',
-    //         instance: 1,
-    //         net: '#I2C-0.SDA',
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     type: 'I2C',
-    //     wire: 'SCL',
-    //     connect: [
-    //       {
-    //         schematic: 'atmega328.sch',
-    //         instance: 1,
-    //         net: '#I2C-0.SCL!||#ADC-5!',
-    //       },
-    //       {
-    //         schematic: 'temperature_sensor.sch',
-    //         instance: 1,
-    //         net: '#I2C-0.SCL',
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     type: 'GPIO',
-    //     wire: null,
-    //     connect: [
-    //       {
-    //         schematic: 'temperature_sensor.sch',
-    //         instance: 1,
-    //         net: '#GPIO-ALERT!',
-    //       },
-    //       {
-    //         schematic: 'atmega328.sch',
-    //         instance: 1,
-    //         net: '#GPIO-3!',
-    //       },
-    //     ],
-    //   },
-    // ];
-    if (data == null) return;
-    try {
-      const serverResult = await server.post('generatePCB', {
-        schData: data,
-      });
-      console.log('Server result', serverResult);
-      if (serverResult.data.schematic != null) {
-        var file = new File([serverResult.data.schematic], 'merged.sch', {
-          type: 'text/plain;charset=utf-8',
-        });
-        FileSaver.saveAs(file);
-      } else {
-        console.error('TODO, output file error');
-      }
-    } catch (e: any) {
-      tschedaFlow.displayError(e.toString());
-    }
+// // Generate Schematic
+// document
+//   .querySelector('#buttonGenerateSchematic')!
+//   .addEventListener('click', async () => {
+//     const data = tschedaFlow.generateJSONSchematic();
+//     console.log(JSON.stringify(data, null, 4));
+//     // const data = [
+//     //   {
+//     //     type: 'VOUT',
+//     //     wire: null,
+//     //     connect: [
+//     //       {
+//     //         schematic: 'power5V12V.sch',
+//     //         instance: 1,
+//     //         net: '@VOUT_5V-12V',
+//     //       },
+//     //       {
+//     //         schematic: 'power5V.sch',
+//     //         instance: 1,
+//     //         net: '@VIN_5V-15V',
+//     //       },
+//     //     ],
+//     //   },
+//     //   {
+//     //     type: 'VOUT',
+//     //     wire: null,
+//     //     connect: [
+//     //       {
+//     //         schematic: 'power5V.sch',
+//     //         instance: 1,
+//     //         net: '@VOUT_5V',
+//     //       },
+//     //       {
+//     //         schematic: 'temperature_sensor.sch',
+//     //         instance: 1,
+//     //         net: '@VIN_2.7V-5.5V',
+//     //       },
+//     //     ],
+//     //   },
+//     //   {
+//     //     type: 'I2C',
+//     //     wire: 'SDA',
+//     //     connect: [
+//     //       {
+//     //         schematic: 'atmega328.sch',
+//     //         instance: 1,
+//     //         net: '#I2C-0.SDA!||#ADC-4!',
+//     //       },
+//     //       {
+//     //         schematic: 'temperature_sensor.sch',
+//     //         instance: 1,
+//     //         net: '#I2C-0.SDA',
+//     //       },
+//     //     ],
+//     //   },
+//     //   {
+//     //     type: 'I2C',
+//     //     wire: 'SCL',
+//     //     connect: [
+//     //       {
+//     //         schematic: 'atmega328.sch',
+//     //         instance: 1,
+//     //         net: '#I2C-0.SCL!||#ADC-5!',
+//     //       },
+//     //       {
+//     //         schematic: 'temperature_sensor.sch',
+//     //         instance: 1,
+//     //         net: '#I2C-0.SCL',
+//     //       },
+//     //     ],
+//     //   },
+//     //   {
+//     //     type: 'GPIO',
+//     //     wire: null,
+//     //     connect: [
+//     //       {
+//     //         schematic: 'temperature_sensor.sch',
+//     //         instance: 1,
+//     //         net: '#GPIO-ALERT!',
+//     //       },
+//     //       {
+//     //         schematic: 'atmega328.sch',
+//     //         instance: 1,
+//     //         net: '#GPIO-3!',
+//     //       },
+//     //     ],
+//     //   },
+//     // ];
+//     if (data == null) return;
+//     try {
+//       const serverResult = await server.post('generatePCB', {
+//         schData: data,
+//       });
+//       console.log('Server result', serverResult);
+//       if (serverResult.data.schematic != null) {
+//         var file = new File([serverResult.data.schematic], 'merged.sch', {
+//           type: 'text/plain;charset=utf-8',
+//         });
+//         FileSaver.saveAs(file);
+//       } else {
+//         console.error('TODO, output file error');
+//       }
+//     } catch (e: any) {
+//       tschedaFlow.displayError(e.toString());
+//     }
+//   });
+
+// // Print map
+// document.querySelector('#buttonPrintMap')!.addEventListener('click', () => {
+//   tschedaFlow.printConnectionMap();
+// });
+
+// // Check DRC
+// document.querySelector('#buttonCheckDrc')!.addEventListener('click', () => {
+//   try {
+//     tschedaFlow.checkDRC();
+//     console.log('Success no errors');
+//   } catch (e: any) {
+//     tschedaFlow.displayError(e.toString());
+//   }
+// });
+
+// Schematic Blocks Panel Tabs
+const panelTabs = document.querySelector('#panelTabs')!.children;
+
+Object.entries(panelTabs).map((tab) => {
+  const tabElement = tab[1];
+
+  tabElement.addEventListener('click', (e) => {
+    const ele = <HTMLElement>e.target;
+    const eleTabName = ele.getAttribute('data-block');
+    // Clean previous Element
+    const previousEle = document.querySelector('#panelTabs .is-active')!;
+    previousEle.classList.remove('is-active'); // Remove tab active
+    const previousTabName = previousEle.getAttribute('data-block')!;
+    (<HTMLElement>(
+      document.querySelector(`[data-box="${previousTabName}"]`)
+    ))!.style.display = 'none'; // Hide box
+    // Set new is-active
+    ele.classList.add('is-active');
+    (<HTMLElement>(
+      document.querySelector(`[data-box="${eleTabName}"]`)
+    ))!.style.display = 'inherit'; // Hide box
+    // (<HTMLElement>(
+    //   document.querySelector(`[data-box="${eleTabName}"]`)
+    // ))!.style.transition = '';
+    // (<HTMLElement>(
+    //   document.querySelector(`[data-box="${eleTabName}"]`)
+    // ))!.style.transition = 'opacity 1s ease-in';
   });
-
-// Print map
-document.querySelector('#buttonPrintMap')!.addEventListener('click', () => {
-  tschedaFlow.printConnectionMap();
+  // Here, object = Array[index, object] (object is the
+  // HTML element object). This means that the actual element
+  // is stored in object[1], not object. Do whatever you need
+  // with it here. In this case we attach a click event:
+  // object[1].addEventListener('click', function () {
+  //   // Output innerHTML of the clicked element
+  //   console.log(
+  //     'Hello ' + this + ' (' + this.innerHTML + ') from map method...',
+  //   );
+  // });
 });
 
-// Check DRC
-document.querySelector('#buttonCheckDrc')!.addEventListener('click', () => {
-  try {
-    tschedaFlow.checkDRC();
-    console.log('Success no errors');
-  } catch (e: any) {
-    tschedaFlow.displayError(e.toString());
-  }
-});
+// document.querySelector('#buttonCheckDrc')!.addEventListener('click', () => {
+//   try {
+//     tschedaFlow.checkDRC();
+//     console.log('Success no errors');
+//   } catch (e: any) {
+//     tschedaFlow.displayError(e.toString());
+//   }
+// });
